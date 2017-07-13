c831bb4d59187ae721e9efa83596c75c4a115e25
BTL-A-30 <abhishek.akbari@gmail.com>
2017-07-13 11:22:09 +0530

    add Basemodel

--- app/BaseModel.php
+++ app/BaseModel.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class BaseModel extends Model
 {
     protected $hidden = ['creator_id', 'updater_id', 'created_at', 'updated_at', 'deleted_at'];
